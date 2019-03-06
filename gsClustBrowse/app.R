77bb070fc1ec499dd12cf15812272b47a4f49615
Murray Cadzow <murray.cadzow@otago.ac.nz>
2019-03-07 09:41:37 +1300

    added tabs for plot and to view the available markers

--- gsClustBrowse/app.R
+++ gsClustBrowse/app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(DT)
 library(tidyverse)
 library(dbplyr)
 library(DBI)
@@ -28,32 +29,54 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("Browse Intensities"),
+    mainPanel(
+        tabsetPanel(
+            tabPanel(title = "Plots",
+                     # Sidebar with a slider input for number of bins
+                     sidebarLayout(
+                         sidebarPanel(
+                             radioButtons("filter_by", "Use Marker Name or Position",
+                                          c("Marker name","Chr and position")),
+                             textInput("markername", label = "Marker name", value = "1KG_1_14106394"),
+                             numericInput("chr", label = "Chr", value = 1),
+                             numericInput("pos", label = "Position", value = 14106394)
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            radioButtons("filter_by", "Use Marker Name or Position",
-                         c("Marker name","Chr and position")),
-            textInput("markername", label = "Marker name", value = "1KG_1_14106394"),
-            numericInput("chr", label = "Chr", value = 1),
-            numericInput("pos", label = "Position", value = 14106394)
+                         ),
 
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("intensityPlot")
+                         # Show a plot of the generated distribution
+                         mainPanel(
+                             plotOutput("intensityPlot")
+                         )
+                     )
+            ),
+            tabPanel(title = "Marker Table",
+                     mainPanel(
+                         numericInput("markerinfo_chr", value = 1, min = 1, label = "Chr"),
+                         dataTableOutput("marker_info")
+                     )
+            )
         )
+
     )
 )
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    # marker table
     markerinfo_tbl <- tbl(con, "marker_info")
+    # joined dataset
     combined_tbl <- tbl(con, "combined")
 
+    # return the marker table from the db so that markers can be browsed
+    output$marker_info <- renderDataTable(
+        datatable({markerinfo_tbl %>%
+            select(chr, position, name) %>%
+            filter(chr == input$markerinfo_chr) %>%
+            arrange(position) %>%
+            as_tibble() }))
 
-
+    # PLot of the intensities for the chosen marker
     output$intensityPlot <- renderPlot({
         if(input$filter_by == "Marker name"){
             filtered_data <- combined_tbl %>% filter(name == input$markername)
