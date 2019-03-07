ae49f4612ef293c2a9fee2451ab17c92bf452305
Murray Cadzow <murray.cadzow@otago.ac.nz>
2019-03-07 18:17:05 +1300

    Point and click from marker tableto make plot
    Optimised so that entire tables aren't loaded in at startup

--- gsClustBrowse/app.R
+++ gsClustBrowse/app.R
@@ -29,34 +29,20 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("Browse Intensities"),
+
+
     mainPanel(
-        tabsetPanel(
-            tabPanel(title = "Plots",
-                     # Sidebar with a slider input for number of bins
-                     sidebarLayout(
-                         sidebarPanel(
-                             radioButtons("filter_by", "Use Marker Name or Position",
-                                          c("Marker name","Chr and position")),
-                             textInput("markername", label = "Marker name", value = "1KG_1_14106394"),
-                             numericInput("chr", label = "Chr", value = 1),
-                             numericInput("pos", label = "Position", value = 14106394)
+         numericInput("markerinfo_chr", value = 1, min = 1, label = "Chr"),
+                  numericInput("markerinfo_start", value = 1, min = 1, label = "Start"),
+                  numericInput("markerinfo_end", value = 1e6, min = 1, label = "End"),
 
-                         ),
+        dataTableOutput("markerinfo"),
+        #dataTableOutput("testTable")
+        plotOutput("intensityPlot")
 
-                         # Show a plot of the generated distribution
-                         mainPanel(
-                             plotOutput("intensityPlot")
-                         )
-                     )
-            ),
-            tabPanel(title = "Marker Table",
-                     mainPanel(
-                         numericInput("markerinfo_chr", value = 1, min = 1, label = "Chr"),
-                         dataTableOutput("markerinfo"),
-                         verbatimTextOutput(outputId = "cell")
-                     )
-            )
-        )
+
+
+
 
     )
 )
@@ -66,7 +52,7 @@ ui <- fluidPage(
 server <- function(input, output) {
     # marker table
     markerinfo_tbl <- reactive(tbl(con, "marker_info") %>%  select(chr, position, name) %>%
-                                   filter(chr == input$markerinfo_chr) %>%
+                                   filter(chr == input$markerinfo_chr, between(position, input$markerinfo_start, input$markerinfo_end)) %>%
                                    arrange(position) #%>% as_tibble()
     )
     # joined dataset
@@ -78,20 +64,31 @@ server <- function(input, output) {
                   rownames = FALSE))
 
     filtered_data <- reactive({
-        coords <- c(chr = input$chr, pos = input$pos)
 
-        if(input$filter_by == "Marker name"){
-            tmp_coords <- markerinfo_tbl() %>% filter(name == input$markername) %>% collect()
-            #coords <- c(chr = tmp_coords$chr[[1]], pos = tmp_coords$pos[[1]])
-        } # has to be position
-        combined_tbl %>% filter(chr == coords[["chr"]] & position == coords[["pos"]]) %>% as_tibble()
+        marker <- "exm101"
+        if(!is.null(input$markerinfo_cell_clicked$col)){
+            #     output$text <- renderPrint(colnames(markerinfo_tbl()))
+            if(colnames(markerinfo_tbl())[input$markerinfo_cell_clicked$col +1 ] == "name"){
+                marker <- input$markerinfo_cell_clicked$value
+            }
+        }
+        #output$text <- renderPrint(cell)
+
+        marker_detail <- markerinfo_tbl() %>% filter(name == marker) %>% collect()
+        output$text <- renderPrint(marker_detail)
+        combined_tbl %>% filter(chr %in% marker_detail$chr, position %in% marker_detail$position) %>% collect()
+
+
 
     })
 
+    output$testTable <- renderDataTable({DT::datatable(filtered_data() %>% head())})
+
     # PLot of the intensities for the chosen marker
     output$intensityPlot <- renderPlot({
-        plot_title <- filtered_data()[["name"]][1]
-        filtered_data()  %>% ggplot(aes(x = x, y = y, colour = gtype)) + geom_point() + ggtitle(plot_title)
+        dat <- filtered_data()
+        plot_title <- dat[["name"]][1]
+        filtered_data()  %>% ggplot(aes(x = x, y = y, colour = gtype)) + geom_point() #+ ggtitle(plot_title)
     })
 
     observeEvent(input$markerinfo_cell_clicked, {
