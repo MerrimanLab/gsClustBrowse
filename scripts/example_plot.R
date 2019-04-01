8ea22dcd59db38092723945a8070c4d60f2db41f
Murray Cadzow <murray.cadzow@otago.ac.nz>
2019-04-02 09:55:00 +1300

    examples added for: pulling out data by marker plotting single marker plotting multiple markers

--- scripts/example_plot.R
+++ scripts/example_plot.R
@@ -4,6 +4,7 @@ library(DBI)
 library(odbc)
 library(config)
 
+
 # get database configuration info from the config.yml file
 dsn <- get("gsint")
 # connect to the database
@@ -12,5 +13,24 @@ con <- dbConnect(odbc::odbc(),driver = dsn$driver, database = dsn$database , tim
 # combined is a database view that has the table joins already specified
 combined_tbl <- tbl(con, "combined")
 
-# query the intensities for a marker
-combined_tbl %>% filter(name == "1KG_1_14106394") %>% ggplot(aes(x = x, y = y, colour = gtype)) + geom_point()
+
+#query intensities for a marker
+combined_tbl %>% filter(name =="1KG_1_14106394") %>% head()
+
+# plot function for a single marker
+plot_intensities <- function(marker){
+  combined_tbl %>% filter(name == marker) %>%
+    ggplot(aes(x = x, y = y, colour = gtype)) +
+    geom_point() +
+    ggtitle(marker) +
+    theme_bw() +
+    expand_limits(x = c(0,1), y = c(0,1))
+}
+
+
+# plot the intensities for a marker
+plot_intensities(marker = "1KG_1_14106394")
+
+# plot multiple markers
+markers <- c("1KG_1_14106394", "exm101", "exm104")
+purrr::map(markers, ~ plot_intensites(marker = .x))
