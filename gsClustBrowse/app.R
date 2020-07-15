d8d136322c22696ad4b0622faf857134bb6734a4
Murray Cadzow <murray.cadzow@otago.ac.nz>
2020-07-15 13:24:18 +1200

    update app to reflect the new columns in the sample table

--- gsClustBrowse/app.R
+++ gsClustBrowse/app.R
@@ -18,13 +18,20 @@ library(config)
 # get database configuration info from the config.yml file
 dsn <- get("psql_ro")
 # connect to the database
-con <- dbConnect(odbc::odbc(),driver = dsn$driver, database = dsn$database, servername = dsn$server, port = dsn$port, UID = dsn$user, PWD = dsn$password , timeout = 100)
+con <- dbConnect(odbc::odbc(),
+                 driver = dsn$driver,
+                 database = dsn$database,
+                 servername = dsn$server,
+                 port = dsn$port,
+                 UID = dsn$user,
+                 PWD = dsn$password,
+                 timeout = 100)
 
-pops <- tbl(con, "sample") %>% select(ancestry) %>%  mutate(ancestry = ifelse(is.na(ancestry),"Missing", ancestry)) %>%  pull(ancestry) %>% unique()
+pops <- tbl(con, "sample") %>% select( ancestry_specific) %>%  mutate(ancestry_specific = ifelse(is.na(ancestry_specific),"Missing", ancestry_specific)) %>%  pull(ancestry_specific) %>% unique()
 batch <- tbl(con, "batch") %>% pull(batchid)
 
 # define genotype colour scale
-gt_colours <- scale_color_manual( values=c("NC" = "grey", "BB" = "green", "AB" = "blue", "AA" ="red"))
+gt_colours <- scale_color_manual( values = c("NC" = "grey", "BB" = "green", "AB" = "blue", "AA" = "red"))
 sex_colours <- scale_color_manual(values = c("Female" = "orange", "Male" = "purple", "Missing" = "lightgrey", "Unknown" = "lightgrey"))
 
 onStop(function() {
@@ -57,8 +64,8 @@ ui <- fluidPage(
             tabPanel("Plot options",
 
                      radioButtons("coord_options", label = "Axes", choices = list(`Theta/R` = "theta_r", `X/Y` = "xy", `X/Y Raw` = "xy_raw")  ),
-                     radioButtons("facet_options", label = "Facet By", choices = list(None = "none", Ancestry = "ancestry", `Reported Sex` = "reported_sex", `Genetic Sex` = "genetic_sex")),
-                     radioButtons("colour_options", label = "Colour", choices = list(Gtype = "gtype", `Reported Sex` = "reported_sex", `Genetic Sex` = "genetic_sex", Ancestry = "ancestry")   )
+                     radioButtons("facet_options", label = "Facet By", choices = list(None = "none", Ancestry = "ancestry_specific", `Reported Sex` = "reported_sex", `Genetic Sex` = "genetic_sex")),
+                     radioButtons("colour_options", label = "Colour", choices = list(Gtype = "gtype", `Reported Sex` = "reported_sex", `Genetic Sex` = "genetic_sex", Ancestry = "ancestry_specific")   )
                      , width = 3)
         )
     ),
@@ -77,7 +84,7 @@ ui <- fluidPage(
 )
 
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
     # marker table
     markerinfo_tbl_pos <- reactive(tbl(con, "marker_info") %>%  select(chr, position, name) %>%
@@ -88,7 +95,7 @@ server <- function(input, output) {
     markerinfo_tbl_probe <- reactive(
         {mkr_tbl <- tbl(con, "marker_info") %>%  select(chr, position, name) %>% head(0) %>%  select(name, chr, position) %>% arrange(position)
 
-        if(!is.null(input$probe_id)  & input$probe_id != ""){
+        if (!is.null(input$probe_id)  & input$probe_id != "") {
             mkr_tbl <- tbl(con, "marker_info") %>%  select(chr, position, name) %>%
                 filter(str_detect(name, !!input$probe_id)) %>%  select(name, chr, position) %>%
                 arrange(name) #%>% as_tibble()
@@ -98,13 +105,12 @@ server <- function(input, output) {
     )
     # joined dataset
     combined_tbl <- tbl(con, "combined")
-    #coord <- str_replace_all(input$coord, "-|:", " ") %>% str_split(., " ") %>% unlist() %>% set_names(., c("chr", "start", "end"))
 
 
     # return the marker table from the db so that markers can be browsed
     output$markerinfo <- renderDataTable(
 
-        datatable({ markerinfo_table_pos_or_probe() %>%collect()}
+        datatable({markerinfo_table_pos_or_probe() %>% collect()}
                   , selection = list( target = "row", mode = "single"),
                   rownames = FALSE)
     )
@@ -115,9 +121,9 @@ server <- function(input, output) {
 
         # initially set marker to be *something*
         marker <- "exm101"
-        if(!is.null(input$markerinfo_cell_clicked$col)){
+        if (!is.null(input$markerinfo_cell_clicked$col)) {
             # if the marker table has been clicked, set the marker to be the marker name that was clicked
-            if(colnames(marker_tbl)[input$markerinfo_cell_clicked$col +1 ] == "name"){
+            if (colnames(marker_tbl)[input$markerinfo_cell_clicked$col + 1 ] == "name") {
                 marker <- input$markerinfo_cell_clicked$value
             }
         }
@@ -127,19 +133,19 @@ server <- function(input, output) {
         out_dat <- combined_tbl %>% filter(chr %in% !!marker_detail$chr, position %in% !!marker_detail$position) %>% collect()
 
 
-        if(input$sex_filter != "all"){
+        if (input$sex_filter != "all") {
             out_dat <- out_dat %>% filter(genetic_sex == !!input$sex_filter | reported_sex == !!input$sex_filter)
         }
 
-        if(!is.null(input$ancestry_filter)){
-            out_dat <- out_dat %>% mutate(ancestry = ifelse(is.na(ancestry),"Missing", ancestry)) %>% filter(ancestry %in% !!input$ancestry_filter)
+        if (!is.null(input$ancestry_filter)) {
+            out_dat <- out_dat %>% mutate(ancestry_specific = ifelse(is.na(ancestry_specific),"Missing", ancestry_specific)) %>% filter(ancestry_specific %in% !!input$ancestry_filter)
         }
 
-        if(input$passed_gt_qc){
+        if (input$passed_gt_qc) {
             out_dat <- out_dat %>% filter(passed_gt_qc == 1)
         }
 
-        if(!input$inc_sample_callrate_lt_95){ # when TRUE remove samples with callrate < 95%
+        if (!input$inc_sample_callrate_lt_95) { # when TRUE remove samples with callrate < 95%
             out_dat <- out_dat %>% filter(callrate > 0.98)
         }
         out_dat %>% mutate(reported_sex = case_when(reported_sex == "Male" ~ "Male", reported_sex == "Female"~ "Female", is.na(reported_sex) | reported_sex == "" ~ "Missing"), genetic_sex = case_when(genetic_sex == "Male" ~ "Male", genetic_sex == "Female"~ "Female", (is.na(genetic_sex) | genetic_sex == "" | genetic_sex == "Unknown" | is.null(genetic_sex)) ~ "Unknown"))
@@ -152,11 +158,11 @@ server <- function(input, output) {
     output$intensityPlot <- renderPlot({
         dat <- filtered_data()#%>% mutate(gtype = forcats::as_factor(gtype) %>% forcats::lvls_expand(.,c("AA","AB","BB", "NC")))
 
-        if(NROW(dat) == 0){
+        if (NROW(dat) == 0) {
             return(NULL)
         }
 
-        if(!is.null(input$batch_filter)){
+        if (!is.null(input$batch_filter)) {
             dat <- dat %>% filter(batchid %in% !!input$batch_filter)
         }
         selected_colour <- input$colour_options
@@ -164,14 +170,14 @@ server <- function(input, output) {
 
         # coordinate plotting options
         p <- dat  %>% ggplot(aes(x = theta, y = r))
-        if(input$coord_options == "xy"){
+        if (input$coord_options == "xy") {
             p <-  dat  %>% ggplot(aes(x = x, y = y))
-        } else if(input$coord_options == "xy_raw"){
+        } else if (input$coord_options == "xy_raw") {
             p <-  dat  %>% ggplot(aes(x = x_raw, y = y_raw))
         }
 
         # facetting
-        if(input$facet_options == "none"){
+        if (input$facet_options == "none") {
             p <- p + facet_wrap(~ batchid)
         } else {
             facet <- paste0(input$facet_options, "~ batchid")
@@ -179,10 +185,12 @@ server <- function(input, output) {
         }
 
         # colours
-        if(input$colour_options == "gtype"){
-            p <- p + geom_point(aes_string(colour = input$colour_options), alpha = 0.7) + gt_colours
-        } else {
-            p <-p + geom_point(aes_string(colour = input$colour_options), alpha = 0.7) + sex_colours
+
+        p <- p + geom_point(aes_string(colour = input$colour_options), alpha = 0.7)
+        if (input$colour_options == "gtype") {
+            p <- p + gt_colours
+        } else if (input$colour_options %in% c("reported_sex", "genetic_sex")) {
+            p <- p + sex_colours
         }
 
         p <- p + ggtitle(plot_title) + theme_bw() + expand_limits(x = c(0,1), y = c(0,1))
@@ -191,9 +199,9 @@ server <- function(input, output) {
     })
 
     markerinfo_table_pos_or_probe <- reactive(
-        { if(input$marker_filter == "position"){
+        {if (input$marker_filter == "position") {
             tab <- markerinfo_tbl_pos()
-        }else{
+        } else {
             tab <- markerinfo_tbl_probe()
         }
             tab
@@ -204,8 +212,8 @@ server <- function(input, output) {
     # watches for the marker table to be clicked and updates everything
     observeEvent(input$markerinfo_cell_clicked, {
         cell <- input$markerinfo_cell_clicked
-        if(!is.null(cell$value)){
-            if(colnames(markerinfo_table_pos_or_probe())[[cell$col +1 ]] == "name"){
+        if (!is.null(cell$value)) {
+            if (colnames(markerinfo_table_pos_or_probe())[[cell$col + 1 ]] == "name") {
                 output$cell <- renderPrint(cell)
             }}
     })
